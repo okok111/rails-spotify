03eac3fdd73b777370d4dcf3b38facd4fdd6b8ca
okok111 <1for.rails@gmail.com>
2024-06-24 21:54:55 +0900

    楽曲検索実装

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,8 +3,12 @@ class SongsController < ApplicationController
   RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
 
   def search
-    if params[:search].present?
-      @searchartists = RSpotify::Artist.search(params[:search])
+    if params[:search_artist].present?
+      @searchartists = RSpotify::Artist.search(params[:search_artist])
+    end
+
+    if params[:search_track].present?
+      @searchtracks = RSpotify::Track.search(params[:search_track])
     end
   end
 
