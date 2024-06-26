bee0a616e2953472d7d2dea4de9d026bcd386637
okok111 <1for.rails@gmail.com>
2024-06-26 21:45:53 +0900

    楽曲投稿実装遷移まで実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,7 +49,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-      params.require(:post).permit(:title, :contents ,:image,:artist_name, :album_name, :track_name, :album_image, :preview_url)
+      params.require(:post).permit(:title, :contents ,:image,:artist_name, :album_name, :track_name, :album_image, :preview_url, :artist_id, :album_id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_26_051023) do
+ActiveRecord::Schema.define(version: 2024_06_26_123221) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2024_06_26_051023) do
     t.string "artist_name"
     t.string "album_name"
     t.string "track_name"
+    t.string "artist_id"
+    t.string "album_id"
   end
 
   create_table "users", force: :cascade do |t|
