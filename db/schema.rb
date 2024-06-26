85c943d629883907c8f038d3b7790cfc6fe101d6
okok111 <1for.rails@gmail.com>
2024-06-26 20:46:15 +0900

    楽曲投稿実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_090020) do
+ActiveRecord::Schema.define(version: 2024_06_26_051023) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 2022_05_25_090020) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "album_image"
+    t.string "preview_url"
+    t.string "artist_name"
+    t.string "album_name"
+    t.string "track_name"
   end
 
   create_table "users", force: :cascade do |t|
